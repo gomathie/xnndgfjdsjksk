69bab8707e00fdb5fd4390398e2a9f305ea33966
gomathie <gomathie@byui.edu>
2023-05-04 17:53:13 +0000

    modified:   index.html     new file:   lesson 2/css/large.css 	new file:   lesson 2/css/medium.css 	new file:   lesson 2/css/normalize.css 	new file:   lesson 2/css/small.css 	modified:   lesson 2/design-principles.html 	new file:   lesson 2/images/apple.com.png 	new file:   lesson 2/images/medium.com.png 	new file:   lesson 2/images/www.softwareadvice.com.png 	modified:   scripts/script.js 	modified:   styles/main.css

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 document.querySelector("#year").innerHTML = "&copy; 2023 | Mathias Lago | Accra Ghana";
 
     let lastUpdated = new Date(document.lastModified);
-    document.getElementById("lastUpdated").textContent = lastUpdated;
+    let formattedDate = lastUpdated.toLocaleString();
+    document.getElementById("lastUpdated").textContent = formattedDate;
   
   
